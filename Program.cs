dd99afbcbceb28c3f0304624c73565e1c97ddfdd
LeeJaeWon816 <we54545yu@gmail.com>
2025-02-22 18:49:45 +0900

    LeeJaeWon

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
+            // 변수의 선언과 데이터 타입
+            // 정수형 데이터
+
+            Console.WriteLine("Hello World!");
+
+            string myName = ("Hello");
+            Console.WriteLine(myName);
+
+            int age = 29;
+            Console.WriteLine(age);
+
+            int myInt = 25;
+            short myShort = 26;
+            long myLong = 27;
+            byte myByte = 28;
+
+            Console.WriteLine(myInt);
+            Console.WriteLine(myShort);
+            Console.WriteLine(myLong);
+            Console.WriteLine(myByte);
+
         }
     }
 }
