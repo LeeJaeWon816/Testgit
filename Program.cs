ae03d294878a9c6e3091b51c91c972be883e2156
LeeJaeWon816 <we54545yu@gmail.com>
2025-03-02 18:53:01 +0900

    20250302 수업자료

--- Program.cs
+++ Program.cs
@@ -4,13 +4,56 @@
     {
         static void Main(string[] args)
         {
-            // Parse
-            // 문자열 데이터를 int, double 과 같은 다른 데이터 타입으로 변환시키는 기능
-            Console.Write("숫자를 입력해주세요 : ");
-            string input = Console.ReadLine();
-            // input에 들어온 값을 숫자로 변환하고 싶다.
-            int value = int.Parse(input);
-            Console.WriteLine(value);
+            // 놀이동산 입장권
+            // 사용자로부터 나이, 군인 할인, 연간 회원 여부를 입력받아서 입장 요금을 계산합니다.
+
+            // 무료입장 조건
+            // 1. 나이가 3세 이하
+            // 2. 나이가 65세 이상
+            // 3. 연간 회원
+
+            // 할인 적용
+            // 1. 군인일 경우 => 30% 할인
+
+            // 일반 요금
+            // 성인(19~64세) : 10000원
+            // 청소년(13~18세) : 8000원
+            // 어린이(4~12세) : 5000원
+
+            // 출력 예시
+            // 나이를 입력하세요. : 20
+            // 군인인가요? (네/아니오) : 네
+            // 연간 회원 인가요? (네/아니오) : 아니오
+            // 무료 입장 입니다.
+
+            Console.Write("나이를 입력해주세요 : ");
+            string ageInput = Console.ReadLine();
+            Console.Write("연간 회원 인가요? (네/아니오) : ");
+            string yuser = Console.ReadLine();
+            int age = int.Parse(ageInput);
+            if (age >= 4 && age <= 12)
+            {
+                Console.WriteLine("어린이");
+            }
+            else if (age >= 13 && age <= 18)
+            {
+                Console.WriteLine("청소년");
+            }
+            else if (age >= 19 && age <= 64)
+            {
+                Console.WriteLine("성인");
+            }
+            else
+            {
+                Console.WriteLine("무료입장 입니다.");
+            }
+
+            Console.Write("군인인가요? (네/아니오) : ");
+            string soldier = Console.ReadLine();
+            if (soldier == "네")
+            {
+                age = ;
+            }
         }
     }
 }
